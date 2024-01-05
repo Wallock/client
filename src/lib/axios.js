28ca457e3fedd130bdae80b7753bf9fc0baab81c
Wallock <wallock32@gmail.com>
2024-01-05 17:28:43 +0700

    again update cors

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axiosInstance = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    baseURL: 'https://api.wb.in.th',
     withCredentials: true,
 })
 
