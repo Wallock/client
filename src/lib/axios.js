5adaafcc3732963afaf323f48640a83d9ce40903
Wallock <wallock32@gmail.com>
2024-01-05 17:24:07 +0700

    test new axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,35 @@
 import Axios from 'axios'
 
-const axios = Axios.create({
+const axiosInstance = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    withCredentials: true,
-    withXSRFToken: true,
+    withCredentials: true, // Send cookies with requests
 })
 
-export default axios
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+    config => {
+        // Add CSRF token to the headers if it's available
+        const csrfToken = getCSRFToken() // Implement a function to get your CSRF token
+        if (csrfToken) {
+            config.headers['X-XSRF-TOKEN'] = csrfToken
+        }
+        return config
+    },
+    error => {
+        return Promise.reject(error)
+    },
+)
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+    response => {
+        // Handle successful responses
+        return response
+    },
+    error => {
+        // Handle errors
+        return Promise.reject(error)
+    },
+)
+
+export default axiosInstance
