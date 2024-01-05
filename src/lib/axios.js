31ac0c60c8594f046f5a5f5494c5029e6d38d8b4
Wallock <wallock32@gmail.com>
2024-01-05 17:26:17 +0700

    Fixed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,14 +2,22 @@ import Axios from 'axios'
 
 const axiosInstance = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    withCredentials: true, // Send cookies with requests
+    withCredentials: true,
 })
+
+// Placeholder function to retrieve CSRF token
+const getCSRFToken = () => {
+    // Implement your logic to retrieve the CSRF token
+    // For example, if the token is stored in a meta tag:
+    const metaTag = document.querySelector('meta[name="csrf-token"]')
+    return metaTag ? metaTag.content : null
+}
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
     config => {
         // Add CSRF token to the headers if it's available
-        const csrfToken = getCSRFToken() // Implement a function to get your CSRF token
+        const csrfToken = getCSRFToken()
         if (csrfToken) {
             config.headers['X-XSRF-TOKEN'] = csrfToken
         }
