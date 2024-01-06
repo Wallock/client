132dd2906bc780e8ddd72b54d8a37391127ac420
Wallock <wallock32@gmail.com>
2024-01-06 11:40:54 +0700

    original file

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -17,7 +17,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
             }),
     )
 
-    const csrf = () => axios.get('/csrf-cookie')
+    const csrf = () => axios.get('/sanctum/csrf-cookie')
 
     const register = async ({ setErrors, ...props }) => {
         await csrf()
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,7 +5,7 @@ const axios = Axios.create({
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
-    withCredentials: false,
+    withCredentials: true,
     withXSRFToken: true,
 })
 
