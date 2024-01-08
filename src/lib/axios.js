0fd24fb20b09e0e51a6fabc24763c1f78da4037a
Wallock <wallock32@gmail.com>
2024-01-08 12:57:27 +0700

    update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -9,28 +9,4 @@ const axios = Axios.create({
     withXSRFToken: true,
 })
 
-axios.interceptors.request.use(
-    async config => {
-        // Fetch CSRF cookie
-        await axios.get('/sanctum/csrf-cookie')
-
-        // Extract XSRF token from the cookie
-        const xsrfToken = getCookie('XSRF-TOKEN')
-
-        // Set the XSRF token in the request headers
-        config.headers['X-XSRF-TOKEN'] = xsrfToken
-
-        return config
-    },
-    error => {
-        return Promise.reject(error)
-    },
-)
-
-// Helper function to get cookie value by name
-function getCookie(name) {
-    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'))
-    if (match) return match[2]
-}
-
 export default axios
