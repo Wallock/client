1aa0f5d5e7a676ce9dc79c61bd48545de6dd5938
Wallock <wallock32@gmail.com>
2024-01-08 12:18:19 +0700

    update Referer to api

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -8,7 +8,11 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
     const [loading, setLoading] = useState(true)
     const { data: user, error, mutate } = useSWR('/api/user', () =>
         axios
-            .get('/api/user')
+            .get('/api/user', {
+                headers: {
+                    Referer: 'https://jssystem.vercel.app',
+                },
+            })
             .then(res => res.data)
             .catch(error => {
                 if (error.response.status !== 409) throw error
