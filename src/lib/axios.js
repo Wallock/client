9c7aa46d87f1e162d7e323d81813d10924ffc1dc
Wallock <wallock32@gmail.com>
2024-01-08 12:47:48 +0700

    update axios token

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -9,4 +9,19 @@ const axios = Axios.create({
     withXSRFToken: true,
 })
 
+axios.interceptors.request.use(
+    config => {
+        // Assuming you have the XSRF token stored in a variable named xsrfToken
+        const xsrfToken = 'your_xsrftoken_value_here'
+
+        // Set the XSRF token in the request headers
+        config.headers['X-XSRF-TOKEN'] = xsrfToken
+
+        return config
+    },
+    error => {
+        return Promise.reject(error)
+    },
+)
+
 export default axios
