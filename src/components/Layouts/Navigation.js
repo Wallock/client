685934d0bf6e898922ab9c4f21f39f72e408f725
Wallock <wallock32@gmail.com>
2024-08-27 16:42:29 +0700

    update old file 6.7.2

--- src/components/Layouts/Navigation.js
+++ src/components/Layouts/Navigation.js
@@ -3,6 +3,7 @@ import Dropdown from '@/components/Dropdown'
 import Link from 'next/link'
 import { ResponsiveNavButton } from '@/components/ResponsiveNavLink'
 import { DropdownButton } from '@/components/DropdownLink'
+import supabase from '@/lib/supabaseClient'
 import { useRouter } from 'next/router'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -11,13 +12,15 @@ import {
     faEllipsisVertical,
     faBullhorn,
 } from '@fortawesome/free-solid-svg-icons'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
 
 const BroadcastAnnouncement = ({ message }) => {
     const [isHovered, setIsHovered] = useState(false)
     return (
         <div className="bg-gray-200 text-black m-auto p-1 font-semibold rounded-xl shadow-md">
-            <div className="px-2 inline-flex">
+            <div className="px-2 inline-flex ">
                 <div className="text-bold text-md text-red-500">
                     <FontAwesomeIcon
                         icon={faBullhorn}
@@ -32,8 +35,7 @@ const BroadcastAnnouncement = ({ message }) => {
                             isHovered ? 'animate-marquee-stopped' : ''
                         }`}
                         onMouseEnter={() => setIsHovered(true)}
-                        onMouseLeave={() => setIsHovered(false)}
-                    >
+                        onMouseLeave={() => setIsHovered(false)}>
                         {message}
                     </div>
                 </div>
@@ -41,22 +43,21 @@ const BroadcastAnnouncement = ({ message }) => {
         </div>
     )
 }
-
 const getBadgeProps = role => {
     switch (role) {
-        case '99':
+        case 99:
             return { color: 'gold', text: 'ผู้พัฒนา' }
-        case '0':
+        case 0:
             return { color: 'ghost', text: 'รอแอดตำแหน่ง' }
-        case '1':
+        case 1:
             return { color: 'primary', text: 'แอดมิน' }
-        case '2':
+        case 2:
             return { color: 'secondary', text: 'เซลล์' }
-        case '3':
+        case 3:
             return { color: 'warning', text: 'ผู้แก้ไข' }
-        case '4':
+        case 4:
             return { color: 'danger', text: 'ผู้จัดการ' }
-        case '5':
+        case 5:
             return { color: 'accent', text: 'ผู้ดูแล' }
         default:
             return { color: 'gray', text: 'Unknown Role' }
@@ -65,65 +66,60 @@ const getBadgeProps = role => {
 
 const Navigation = ({ user, profile }) => {
     const router = useRouter()
-
-    const handleLogout = () => {
-        localStorage.removeItem('accessToken')
+    const handleLogout = async () => {
+        await supabase.auth.signOut()
         router.push('/login')
     }
-
-    const { color, text } = getBadgeProps(profile?.role)
+    const { color, text } = getBadgeProps(profile?.level)
     const [open, setOpen] = useState(false)
     const [announcements, setAnnouncements] = useState(null)
     const [error, setError] = useState(null)
 
     useEffect(() => {
         const fetchAnnouncements = async () => {
-            const token = localStorage.getItem('accessToken')
-
-            if (!token) {
-                router.push('/login')
-                return
-            }
-
             try {
-                const response = await fetch(
-                    'https://beta.wb.in.th/api/announcements',
-                    {
-                        method: 'GET',
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                            'Content-Type': 'application/json',
-                        }
-                    }
-                )
+                const { data: announcementsData, error } = await supabase
+                    .from('announcements')
+                    .select('*')
+                    .order('startTimestamp', { ascending: false })
 
-                if (!response.ok) {
-                    throw new Error('Failed to fetch announcements')
+                if (error) {
+                    throw new Error(error.message)
                 }
 
-                const data = await response.json()
-                setAnnouncements(data)
+                setAnnouncements(
+                    announcementsData.map(announcement => ({
+                        ...announcement,
+                        startTimestamp: new Date(
+                            announcement.startTimestamp,
+                        ).getTime(),
+                        endTimestamp: new Date(
+                            announcement.endTimestamp,
+                        ).getTime(),
+                    })),
+                )
             } catch (error) {
-                setError('Error Connect API')
+                setError(error.message)
             }
         }
 
         fetchAnnouncements()
 
-        // Check current time every 30 seconds
+        // Check current time every minute
         const interval = setInterval(fetchAnnouncements, 30000)
 
         return () => clearInterval(interval)
-    }, [router])
+    }, [])
 
     const currentTimestamp = new Date().getTime()
-
     return (
         <>
             <nav className="bg-white border-b border-gray-100 fixed w-full top-0 z-10 shadow">
+                {/* Primary Navigation Menu */}
                 <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-12">
                         <div className="flex">
+                            {/* Logo */}
                             <div className="flex-shrink-0 flex items-center">
                                 <Link href="/">
                                     <ApplicationLogo className="block h-10 w-auto fill-current" />
@@ -132,11 +128,6 @@ const Navigation = ({ user, profile }) => {
                         </div>
 
                         <div className="hidden sm:flex sm:items-center">
-                            {error && (
-                                <div className="text-red-500 font-bold">
-                                    {error}
-                                </div>
-                            )}
                             {announcements &&
                                 announcements.map(announcement => {
                                     const {
@@ -159,8 +150,8 @@ const Navigation = ({ user, profile }) => {
                                     return null
                                 })}
                         </div>
-
-                        <div className="hidden sm:flex sm:items-center sm:ml-6">
+                        {/* Settings Dropdown */}
+                        <div className="hidden sm:flex sm:items-center sm:ml-6 ">
                             <Dropdown
                                 align="right"
                                 width="48"
@@ -168,8 +159,7 @@ const Navigation = ({ user, profile }) => {
                                 trigger={
                                     <button className="flex items-center  text-gray-600 hover:text-gray-800 focus:outline-none transition duration-150 ease-in-out">
                                         <div
-                                            className={`badge badge-${color} badge-outline mx-1 text-xs font-semibold`}
-                                        >
+                                            className={`badge badge-${color} badge-outline mx-1 text-xs font-semibold`}>
                                             {text}
                                         </div>
                                         <div className="mx-1 font-semibold text-lg">
@@ -183,10 +173,10 @@ const Navigation = ({ user, profile }) => {
                                             />
                                         </div>
                                     </button>
-                                }
-                            >
+                                }>
+                                {/* Authentication */}
                                 <DropdownButton onClick={handleLogout}>
-                                    <div className="flex items-center font-semibold text-lg text-red-600 justify-center">
+                                    <div className="flex items-center font-semibold text-lg text-red-600 justify-center ">
                                         <FontAwesomeIcon
                                             icon={faRightFromBracket}
                                             className="mr-2 h-5 w-5"
@@ -197,17 +187,16 @@ const Navigation = ({ user, profile }) => {
                             </Dropdown>
                         </div>
 
+                        {/* Hamburger */}
                         <div className="-mr-2 flex items-center sm:hidden">
                             <button
                                 onClick={() => setOpen(open => !open)}
-                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                            >
+                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                                 <svg
                                     className="h-6 w-6"
                                     stroke="currentColor"
                                     fill="none"
-                                    viewBox="0 0 24 24"
-                                >
+                                    viewBox="0 0 24 24">
                                     {open ? (
                                         <path
                                             className="inline-flex"
@@ -231,8 +220,18 @@ const Navigation = ({ user, profile }) => {
                     </div>
                 </div>
 
+                {/* Responsive Navigation Menu */}
                 {open && (
                     <div className="block sm:hidden">
+                        {/* <div className="pt-2 pb-3 space-y-1">
+                         <ResponsiveNavLink
+                            href="/dashboard"
+                            active={router.pathname === '/'}>
+                            หน้าหลัก
+                        </ResponsiveNavLink>
+                    </div> */}
+
+                        {/* Responsive Settings Options */}
                         <div className="pt-4 pb-1 border-t border-gray-200">
                             <div className="flex items-center px-4">
                                 <div className="flex-shrink-0">
@@ -244,7 +243,7 @@ const Navigation = ({ user, profile }) => {
 
                                 <div className="ml-3">
                                     <div className="font-medium text-base text-gray-800">
-                                        {profile?.name}
+                                        {user?.name}
                                     </div>
                                     <div className="font-medium text-sm text-gray-500">
                                         {user?.email}
@@ -253,13 +252,14 @@ const Navigation = ({ user, profile }) => {
                             </div>
 
                             <div className="mt-3 space-y-1">
+                                {/* Authentication */}
                                 <ResponsiveNavButton onClick={handleLogout}>
                                     <div className="flex items-center font-semibold rounded-box text-lg bg-red-600 text-white p-1 justify-center">
                                         <FontAwesomeIcon
                                             icon={faRightFromBracket}
                                             className="mr-2 h-5 w-5"
                                         />
-                                        ออกจากระบบ.
+                                        ออกจากระบบ
                                     </div>
                                 </ResponsiveNavButton>
                             </div>
